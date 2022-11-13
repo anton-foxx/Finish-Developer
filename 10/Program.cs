1fd41180cdb9583755cf032f16c55014186ffaca
Антон <anton@MacBook-Air.local>
2022-11-13 14:07:30 +0300

    Написанна программа решаюшая поставленную задачу

---
+++ 10/Program.cs
@@ -0,0 +1,72 @@
+﻿
+// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// Пример:
+// ["hello", "2","world", ":-)"] -> ["2", ":-)"]
+
+// ["1234", "1567", "-2", "long word"] -> ["-2"]
+
+// ["Russia", "Denmark", "Kazan"] -> []
+
+Console.Clear();
+string[] ArrayStrings = new string[] { };
+Console.Write("Введите массив: ");
+int n = int.Parse(Console.ReadLine());
+ArrayStrings = new string[n];
+for (int i = 0; i < ArrayStrings.Length; i++)
+{
+    Console.Write($"Введите {i + 1} элемент массива: ");
+    ArrayStrings[i] = Console.ReadLine();
+}
+Console.WriteLine();
+Console.WriteLine("Ваш массив:");
+Console.WriteLine($"{PrintArray(ArrayStrings)}");
+Console.WriteLine();
+
+int length = 3;
+
+int CheckArray(string[] array, int length)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= length) result++;
+    }
+    return result;
+}
+
+int numbers = CheckArray(ArrayStrings, length);
+
+string[] newArrayStrings = new string[numbers];
+
+NewArray(ArrayStrings, newArrayStrings, length);
+
+void NewArray(string[] oldArray, string[] newArray, int lengthLimit)
+{
+    int temp = 0;
+    for (int i = 0; i < oldArray.Length; i++)
+    {
+        if (oldArray[i].Length <= lengthLimit)
+        {
+            newArray[temp] = oldArray[i];
+            temp++;
+        }
+    }
+}
+
+string PrintArray(string[] array)
+{
+    string result = string.Empty;
+    result = "[ ";
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += $"{array[i],1}";
+        if (i < array.Length - 1) result += ", ";
+    }
+    result += " ]";
+    return result;
+}
+Console.WriteLine("Ваш массив, с элементами длина которых меньше либо равна трем символам:");
+Console.WriteLine($"{PrintArray(newArrayStrings)}");
